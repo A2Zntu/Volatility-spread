3d81ec2b452342fe95c6dfa6c9617fb54860f817
A2Zntu <cyf0531@gmail.com>
2018-11-07 16:34:12 +0800

    makedir_and_movefile
    創造資料夾
    移動檔案到子資料夾

---
+++ makedir_and_movefile.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 14:42:11 2018
+
+@author: user_2
+"""
+
+import os, shutil, glob
+
+path = 'F:/CBOE Options Tick Data/SPX Tick Data/'
+
+
+list_year_and_month = []
+list_year = []
+start_year = 2004
+end_year = 2017
+start_month = 1
+end_month = 12
+
+extension = 'csv'
+
+
+
+for i in range(start_year, end_year + 1):
+    list_year.append(str(i))
+    for j in range(start_month, end_month + 1):
+        if j < 10:
+            list_year_and_month.append(str(i) + '0' + str(j))
+        else:
+            list_year_and_month.append(str(i) +str(j))
+
+
+for i, year in enumerate(range(2004,2017)):
+    for j in range(i*12, (i+1)*12):
+        if not os.path.exists(path + list_year[i] + '/' + list_year_and_month[j]):
+            os.makedirs(path + list_year[i] + '/' + list_year_and_month[j])
+
+
+list_number = []            
+for i in range(14):
+    list_hi = []
+    for j in range(i*12,(i+1)*12):
+        list_hi.append(j)
+    list_number.append(list_hi)
+
+
+    
+#%%
+def right_dir(year, res):
+    loc = list_year.index(year)
+    for j in range(loc*12, (loc+1)*12):
+        a = res.find(list_year_and_month[j])
+        if a != -1:
+            break
+    return list_year_and_month[j]
+    
+
+
+extension = 'gz'
+
+for i, year in enumerate(range(2004,2016)):
+    os.chdir(path + str(year))
+    result = [i for i in glob.glob('*.{}'.format(extension))]
+    for j in range(len(result)):
+        subpath = right_dir(list_year[i], result[j])
+        print(subpath)
+        shutil.move(path + list_year[i] + '/' + result[j], path + list_year[i] + '/' +str(subpath) + '/' + result[j])
+
+
